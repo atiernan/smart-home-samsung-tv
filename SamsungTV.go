a61eb4301bdc3a395c1e61bf07fd55b2ab77a6aa
Alex Tiernan <alex@alex-tiernan.co.uk>
2018-01-06 12:25:47 +0000

    Initial commit

---
+++ SamsungTV.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+var portNumber = 55000
+var webSocketPort = 8001
+
+type SamsungTV struct {
+	Host            string
+	ApplicationID   string
+	ApplicationName string
+	legacySocket    net.Conn
+	webSocket       *websocket.Conn
+	connectedLegacy bool
+	connectedWS     bool
+}
+
+// Connect to a Samsung TV at the given IP address
+func (tv *SamsungTV) Connect() bool {
+	tv.connectedLegacy = false
+	tv.connectedWS = false
+
+	u := url.URL{
+		Scheme:  "ws",
+		Host:    fmt.Sprintf("%s:%d", tv.Host, webSocketPort),
+		Path:    "/api/v2/channels/samsung.remote.control",
+		RawPath: "name=" + tv.ApplicationID,
+	}
+
+	var err error
+	tv.webSocket, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		fmt.Println("Failed to connect to TV websocket, trying legacy connection")
+		tv.legacySocket, err = net.Dial("tcp", fmt.Sprintf("%s:%d", tv.Host, portNumber))
+		if err == nil {
+			tv.connectedLegacy = true
+		} else {
+			fmt.Println("Failed to connect through legacy connection")
+		}
+	} else {
+		_, _, err := tv.webSocket.ReadMessage()
+		if err != nil {
+			log.Println("Error:", err)
+		}
+		tv.connectedWS = true
+	}
+
+	return (tv.connectedLegacy || tv.connectedWS)
+}
+
+// Close the connection to the TV
+func (tv *SamsungTV) Close() {
+	if tv.connectedLegacy {
+		tv.legacySocket.Close()
+		tv.connectedLegacy = false
+	} else if tv.connectedWS {
+		tv.webSocket.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+		tv.webSocket.Close()
+		tv.connectedWS = false
+	}
+}
+
+func (tv SamsungTV) sendLegacyCommand(command string) bool {
+	header := bytes.Buffer{}
+	header.WriteByte(0x64)
+	header.WriteByte(0x00)
+	appendBase64(tv.Host, &header)
+	appendBase64(tv.ApplicationID, &header)
+	appendBase64(tv.ApplicationName, &header)
+
+	tv.legacySocket.Write(wrapMessage(&header))
+
+	cmd := bytes.Buffer{}
+	cmd.Write([]byte{0x00, 0x00, 0x00})
+	appendBase64(command, &cmd)
+
+	tv.legacySocket.Write(wrapMessage(&cmd))
+
+	response := make([]byte, 64)
+	length, err := tv.legacySocket.Read(response)
+	if err != nil {
+		return false
+	}
+	if length > 0 {
+		return true
+	}
+
+	return true
+}
+
+// sendWSCommand sends a command to a TV using a websocket
+func (tv SamsungTV) sendWSCommand(command string) bool {
+	type paramsStruct struct {
+		Cmd          string
+		DataOfCmd    string
+		Option       bool
+		TypeOfRemote string
+	}
+
+	type payLoad struct {
+		Method string       `json:"method"`
+		Params paramsStruct `json:"params"`
+	}
+
+	var msg = &payLoad{
+		Method: "ms.remote.control",
+		Params: paramsStruct{
+			Cmd:          "Click",
+			DataOfCmd:    command,
+			Option:       false,
+			TypeOfRemote: "SendRemoteKey",
+		},
+	}
+
+	err := tv.webSocket.WriteJSON(msg)
+	return (err == nil)
+}
+
+// SendCommand sends a command to the TV, using whichever connection is available
+func (tv SamsungTV) SendCommand(command string) bool {
+	if tv.connectedLegacy {
+		return tv.sendLegacyCommand(command)
+	} else if tv.connectedWS {
+		return tv.sendWSCommand(command)
+	} else {
+		fmt.Println("Not connected to TV")
+	}
+	return false
+}
+
+func wrapMessage(msg *bytes.Buffer) []byte {
+	wrapped := bytes.Buffer{}
+	appName := "iphone..iapp.samsung"
+
+	wrapped.WriteByte(0x00)
+	wrapped.WriteByte(uint8(len(appName)))
+	wrapped.WriteByte(0x00)
+	wrapped.Write([]byte(appName))
+	wrapped.WriteByte(uint8(msg.Len()))
+	wrapped.WriteByte(0x00)
+	wrapped.Write(msg.Bytes())
+	return wrapped.Bytes()
+}
+
+func appendBase64(value string, msg *bytes.Buffer) {
+	encodedValue := []byte(base64.StdEncoding.EncodeToString([]byte(value)))
+	msg.WriteByte(uint8(len(encodedValue)))
+	msg.WriteByte(0x00)
+	msg.Write(encodedValue)
+}
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"TVCommon"
+	"encoding/json"
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/chbmuc/cec"
+)
+
+func main() {
+	tvIP := flag.String("tv-ip", "127.0.0.1", "The IP address of the TV to control")
+	serverURL := flag.String("server", "http://example.com", "The URL to the webserver")
+	flag.Parse()
+
+	if *tvIP == "127.0.0.1" {
+		log.Println("A host IP must be specified")
+		return
+	}
+
+	if *serverURL == "http://example.com" {
+		log.Println("A server URL must be specified")
+		return
+	}
+
+	c, err := cec.Open("", "cec.go")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	tv := SamsungTV{
+		Host:            *tvIP,
+		ApplicationID:   "google-home-samsung",
+		ApplicationName: "Google Samsung Remote",
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+		response, err := http.Get(*serverURL)
+		if err != nil {
+			log.Println("Failed to fetch latest status")
+		}
+
+		decoder := json.NewDecoder(response.Body)
+		var message TVCommon.DeviceEndpointResponse
+		if err := decoder.Decode(&message); err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			if message.TVSwitchedOn {
+				c.PowerOn(0)
+			}
+			if message.TVSwitchedOff {
+				if tv.Connect() {
+					tv.SendCommand("KEY_POWER")
+					tv.Close()
+				}
+			}
+			if message.VolumeUp > 0 {
+				if tv.Connect() {
+					for i := 0; i < message.VolumeUp; i++ {
+						tv.SendCommand("KEY_VOLUP")
+						time.Sleep(500 * time.Millisecond)
+					}
+					tv.Close()
+				}
+			}
+			if message.VolumeDown > 0 {
+				if tv.Connect() {
+					for i := 0; i < message.VolumeDown; i++ {
+						tv.SendCommand("KEY_VOLDOWN")
+						time.Sleep(500 * time.Millisecond)
+					}
+					tv.Close()
+				}
+			}
+			if message.VolumeMute {
+				if tv.Connect() {
+					tv.SendCommand("KEY_MUTE")
+					tv.Close()
+				}
+			}
+			if message.Pause {
+				if tv.Connect() {
+					tv.SendCommand("KEY_PAUSE")
+					tv.Close()
+				}
+			}
+			if message.Play {
+				if tv.Connect() {
+					tv.SendCommand("KEY_PLAY")
+					tv.Close()
+				}
+			}
+		}
+		response.Body.Close()
+	}
+}
